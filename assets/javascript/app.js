5d17d79cf248edf7ec337b1ee1fc4bfffc23a330
GuillermoGahbler <mexamericano@gmail.com>
2018-03-01 16:42:37 -0800

    updated Trivia index.html, style.css, app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,35 @@
 var timer = 0;
-var timesUp =0;
+var timesUp = 0;
 var questionContainerString = $('.questionContainer')["0"].outerHTML
 // console.log(questionContainerString);
 // this will console.log all changes as they happen
- var startSting = $('#start')["0"].outerHTML
+var startSting = $('#start')["0"].outerHTML
 
 var answers = ['Italy', '13%', 'Lombardis Pizzeria', 'Pepperoni', 'Michelangelo']
 
 var time = 25000;
 
 
-
-
 function stoptimer() {
     clearInterval(timer);
     clearTimeout(timesUp);
-    
+
 }
 
 $('body div').html(startSting)
-$('#start').on('click', function(){
-$('body div').html(questionContainerString)
-timer = setInterval(function () {
-    time -= 1000;
-    $('#timer').html('You have '+time/1000+' seconds left')
-}, 1000);
+$('#start').on('click', function () {
+    $('body div').html(questionContainerString)
+    timer = setInterval(function () {
+        time -= 1000;
+        $('#timer').html('You have ' + time / 1000 + ' seconds left')
+    }, 1000);
 
-timesUp = setTimeout(function () {
-stoptimer()
-}, 25000)
+    timesUp = setTimeout(function () {
+        stoptimer()
+    }, 25000)
 
 })
+
+function getResults() {
+ let pizzaQuestions =  $('.pizzaQuestion')    
+}
